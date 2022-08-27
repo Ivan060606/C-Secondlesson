814ebed0d17ae42fe45b1335e65d8e0bd1ddb1f5
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-08-27 23:13:42 +0300

    Example3

---
+++ Example3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("ВВедите цифру, обозначающую день недели");
+int a = Convert.ToInt32(Console.ReadLine());
+if (0<a && a<8)
+    {
+    if (0<a && a<6) Console.WriteLine("Будний день");
+    else Console.WriteLine("Выходной день");
+    }
+else Console.WriteLine("введите корректную цифру");

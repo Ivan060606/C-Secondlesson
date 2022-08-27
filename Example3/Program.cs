bbd66924d772707b1c38f84f7a8140b2e558a4b3
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-08-28 01:45:05 +0300

    Example4

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,8 +1,14 @@
-﻿Console.WriteLine("ВВедите цифру, обозначающую день недели");
+﻿/*Console.WriteLine("ВВедите цифру, обозначающую день недели");
 int a = Convert.ToInt32(Console.ReadLine());
 if (0<a && a<8)
     {
     if (0<a && a<6) Console.WriteLine("Будний день");
     else Console.WriteLine("Выходной день");
     }
-else Console.WriteLine("введите корректную цифру");
+else Console.WriteLine("введите корректную цифру");*/
+
+Console.WriteLine("ВВедите цифру, обозначающую день недели");
+int a = Convert.ToInt32(Console.ReadLine());
+a=a%100;
+a=a%10;
+Console.WriteLine("ВВедите цифру, обозначающую день недели"+a);
---
+++ Example4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("ВВедите количество программистов в комнате ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+Console.Write("В комнате "+b);
+if (100<a) a=a%100;
+if (4<a && a<21) Console.WriteLine(" программистов");
+else
+    {
+    a=a%10;
+    if (a==1) Console.WriteLine(" программист");
+    else if (1<a && a<5) Console.WriteLine(" программистa");
+    else Console.WriteLine(" программистов");
+    }

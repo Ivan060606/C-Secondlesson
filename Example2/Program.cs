7451b42e5a228d8c0dee4f1090bf281cc3e9d056
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-08-27 21:36:46 +0300

    Example2

---
+++ Example2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b/100>0) 
+    {
+    string a = Convert.ToString(b); 
+    Console.WriteLine(a[2]);
+    }
+else
+    {
+    Console.WriteLine("третьей цифры нет");
+    }
